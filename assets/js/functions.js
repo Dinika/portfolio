efbe7cf693d5d1da9ac16ba5131b1de73d0c89a5
Dinika <dinikasaxenas@gmail.com>
2017-03-06 12:53:18 +0530

    Put a notice and a down chevron button

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,7 @@ window.addEventListener("load", function()
   var loadScreen = document.getElementById('loadScreen');
   document.body.removeChild(loadScreen);
 });
+
+if (!document.hidden) {
+    // do what you need
+}
