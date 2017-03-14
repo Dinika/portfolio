8654785001456c81be6b829e00351ed63f2283b1
Dinika <dinikasaxenas@gmail.com>
2017-03-15 01:23:37 +0530

    Hide the header on click

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,15 +6,31 @@ window.addEventListener("load", function()
 
 if (!document.hidden) {
  (function(){
+    var section0 =
+    {
+        home: $('header'),
+        init: function()
+              {
+                var diamond = $('.diamond').addClass('dAnimate');
+                $('.rightMenu .downArrow').on('click', this.hide);
+              },
+        hide: function()
+              {
+                var sh = section0.home;
+                sh.addClass('hExit');
+                section1.init();
+              }
+    }
 
-    var diamond = $('.diamond').addClass('dAnimate');
-    $('.rightMenu .downArrow').on('click', function()
-      {
+    var section1 =
+    {
+        init: function()
+        {
           var element = document.querySelector('.hiddenWaitMessage');
+              element.classList.add('waitMessage');
 
-          element.classList.add('waitMessage');
-
-      });
-
+        }
+    }
+    section0.init();
 })();
 };
