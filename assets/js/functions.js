948a97475d600dba7c647907223c714c69adbea2
Dinika <dinikasaxenas@gmail.com>
2017-03-18 02:46:53 +0530

    Add blog section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -19,22 +19,23 @@ if (!document.hidden) {
                 var sh = section0.home;
                 sh.fadeOut(500);
                 section1.init();
-              }
+              },
     }
 
     var section1 =
     {
-        skills: $('#section1'),
+        skills: $('.section1'),
         init: function()
         {
-            $('#section1').delay(500).fadeIn(1000);
+            $('.section1').delay(500).fadeIn(1000);
             $('.rightMenu .downArrow').on('click', this.hide);
         },
         hide: function()
         {
           section1.skills.fadeOut(300);
+          section1.skills.addClass('skillsHide');
           section2.init();
-        }
+        },
     }
 
     var section2 =
@@ -42,9 +43,24 @@ if (!document.hidden) {
         work: $('#section2'),
         init: function()
         {
-            $('#section2').delay(800).fadeIn(200);
-        }
-    }
+            section2.work.delay(800).fadeIn(200);
+            $('.rightMenu .downArrow').on('click', this.hide);
+        },
+        hide: function()
+        {
+            section2.work.fadeOut(300);
+            section2.work.addClass('skillsHide');
+            section3.init();
+        },
+      }
+      var section3 =
+      {
+          blog: $('#section3'),
+          init: function()
+          {
+              section3.blog.delay(800).fadeIn(200);
+          },
+      }
 
 
     section0.init();
