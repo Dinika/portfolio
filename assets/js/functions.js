5a09ee51e06838d4db485944476101519013a07d
Dinika Saxena <dinika@gssis-dell-e7440-01.dyndns.cern.ch>
2018-10-08 14:09:29 +0200

    change theme colors
    Signed-off-by: Dinika Saxena <dinika@gssis-dell-e7440-01.dyndns.cern.ch>

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -102,11 +102,11 @@ if (!document.hidden) {
         parent: { element: document.body, position: 'append' },
         position: 'fixed',
         lines: [
-          {top: 0, left: '0', width: '20vw', height: '100vh', color: '#F85659', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 0, direction: 'TopBottom' }},
-          {top: 0, left: '20vw', width: '20vw', height: '100vh', color: '#7030a0', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 150, direction: 'BottomTop' }},
-          {top: 0, left: '40vw', width: '20vw', height: '100vh', color: '#F85659', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 300, direction: 'TopBottom' }},
-          {top: 0, left: '60vw', width: '20vw', height: '100vh', color: '#7030a0', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 450, direction: 'BottomTop' }},
-          {top: 0, left: '80vw', width: '20vw', height: '100vh', color: '#F85659', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 600, direction: 'TopBottom' }}
+          {top: 0, left: '0', width: '20vw', height: '100vh', color: '#3c3c3c', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 0, direction: 'TopBottom' }},
+          {top: 0, left: '20vw', width: '20vw', height: '100vh', color: '#E0E0E0', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 150, direction: 'BottomTop' }},
+          {top: 0, left: '40vw', width: '20vw', height: '100vh', color: '#3c3c3c', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 300, direction: 'TopBottom' }},
+          {top: 0, left: '60vw', width: '20vw', height: '100vh', color: '#E0E0E0', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 450, direction: 'BottomTop' }},
+          {top: 0, left: '80vw', width: '20vw', height: '100vh', color: '#3c3c3c', hidden: false, animation: { duration: 1000, easing: 'easeInOutCirc', delay: 600, direction: 'TopBottom' }}
         ]
     });
     
